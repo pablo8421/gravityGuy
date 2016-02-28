3b85ed98c386787add33f46110fae2ef74eb2a50
pablo8421 <pablo_jsd-8@hotmail.com>
2016-02-28 01:01:14 -0600

    Algo.

--- src/GGServer.java
+++ src/GGServer.java
@@ -1,4 +1,5 @@
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.io.BufferedReader;
@@ -9,6 +10,7 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JTextArea;
@@ -239,6 +241,150 @@ public class GGServer extends javax.swing.JFrame implements Runnable {
         public void startGame()
         {
             sendBoth("START GAME");
+        }
+    }
+
+    private class GameState
+    {
+        ArrayList<Cuadrado> cuadradosBottom;
+        ArrayList<Cuadrado> cuadradosTop;
+        Cuadrado jugador1;
+        Cuadrado jugador2;
+        
+        int width, height;
+        
+        public GameState()
+        {
+            cuadradosBottom = new ArrayList();
+            cuadradosTop = new ArrayList();
+            jugador1 = new Cuadrado(320, 320, 100, 100);
+            jugador2 = new Cuadrado(320, 320, 100, 100);
+            width = 640;
+            height = 640;
+        }
+        
+        public void initGame()
+        {
+            Cuadrado cuadrado;
+            int randomx;
+            int randomy;
+            int xAnterior = 0;
+            int x = 0;
+            
+            //PARA ARRIBA
+            while (x < width)
+            {
+                randomx = (int) (Math.random() * (150 - 0)) + 10;
+                randomy = (int) (Math.random() * (150 - 0)) + 10;
+                x = xAnterior;
+                cuadrado = new Cuadrado(x, 0, randomx, randomy);
+                cuadradosTop.add(cuadrado);
+                xAnterior = xAnterior + randomx;
+            }
+
+            //PARA ABAJO, Para abajo Para abajo
+            x = 0;
+            while (x < width)
+            {
+                randomx = (int) (Math.random() * (150 - 0)) + 10;
+                randomy = (int) (Math.random() * (150 - 0)) + 10;
+                x = xAnterior;
+                cuadrado = new Cuadrado(x, height - randomy, randomx, randomy);
+                cuadradosBottom.add(cuadrado);
+                xAnterior = xAnterior + randomx;
+            }
+        }
+        
+        
+        public void updateState()
+        {
+            Cuadrado cuadrado;
+            int cuadroActual=0;
+            int randomx;
+            int randomy;
+            int xAnterior;
+            
+            //PARA ARRIBA
+            for (int i = 0; i < cuadradosBottom.size(); i++)
+            {
+                //g.setColor(Color.white);
+                //g.drawRect(cuadradosBottom.get(i).x0, cuadradosBottom.get(i).y0, cuadradosBottom.get(i).width, cuadradosBottom.get(i).height);
+            }
+            for (int i = 0; i < cuadradosBottom.size(); i++)
+            {
+                cuadradosBottom.get(i).x0 -= 10;
+            }
+            if ((cuadradosBottom.get(0).x0 + cuadradosBottom.get(0).width) <= 0)
+            {
+                cuadradosBottom.remove(0);
+            }
+            if ((cuadradosBottom.get(cuadradosBottom.size() - 1).x0 + cuadradosBottom.get(cuadradosBottom.size() - 1).width) <= width)
+            {
+                randomx = (int) (Math.random() * (150 - 0)) + 10;
+                randomy = (int) (Math.random() * (150 - 0)) + 10;
+                //g.setColor(Color.red);
+                //g.drawRect(x,0,randomx,randomy);
+                xAnterior = cuadradosBottom.get(cuadradosBottom.size() - 1).x0 + cuadradosBottom.get(cuadradosBottom.size() - 1).width;
+                cuadrado = new Cuadrado(xAnterior, 0, randomx, randomy);
+                cuadradosBottom.add(cuadrado);
+            }
+            for (int i = 0; i < cuadradosBottom.size(); i++)
+            {
+                //g.setColor(Color.red);
+                //g.drawRect(cuadradosBottom.get(i).x0, cuadradosBottom.get(i).y0, cuadradosBottom.get(i).width, cuadradosBottom.get(i).height);
+            }
+
+            //PARA ABAJO
+            for (int i = 0; i < cuadradosTop.size(); i++)
+            {
+                //g.setColor(Color.white);
+                //g.drawRect(cuadradosTop.get(i).x0, cuadradosTop.get(i).y0, cuadradosTop.get(i).width, cuadradosTop.get(i).height);
+            }
+            for (int i = 0; i < cuadradosTop.size(); i++)
+            {
+                cuadradosTop.get(i).x0 -= 10;
+            }
+            if ((cuadradosTop.get(0).x0 + cuadradosTop.get(0).width) <= 0)
+            {
+                cuadradosTop.remove(0);
+            }
+            if ((cuadradosTop.get(cuadradosTop.size() - 1).x0 + cuadradosTop.get(cuadradosTop.size() - 1).width) <= width)
+            {
+                randomx = (int) (Math.random() * (150 - 0)) + 10;
+                randomy = (int) (Math.random() * (150 - 0)) + 10;
+                //g.setColor(Color.red);
+                //g.drawRect(x,0,randomx,randomy);
+                xAnterior = cuadradosTop.get(cuadradosTop.size() - 1).x0 + cuadradosTop.get(cuadradosTop.size() - 1).width;
+                cuadrado = new Cuadrado(xAnterior, height - randomy, randomx, randomy);
+                cuadradosTop.add(cuadrado);
+            }
+            for (int i = 0; i < cuadradosTop.size(); i++)
+            {
+                //g.setColor(Color.red);
+                //g.drawRect(cuadradosTop.get(i).x0, cuadradosTop.get(i).y0, cuadradosTop.get(i).width, cuadradosTop.get(i).height);
+                if (cuadradosTop.get(i).x0 <= jugador1.x0 && (cuadradosTop.get(i).x0 + cuadradosTop.get(i).width) >= jugador1.x0)
+                {
+                    cuadroActual = i;
+                }
+            }
+            //g.setColor(Color.white);
+            //g.drawRect(jugador1.x0, jugador1.y0, jugador1.width, jugador1.height);
+            //AQUI SE LLAMA LA GRAVEDAD
+
+            PlayerManage playerm1 = new PlayerManage(jugador1, cuadradosTop, cuadroActual);
+            Thread infThread = new Thread(playerm1);
+            infThread.start();
+
+            //g.setColor(Color.black);
+            //g.drawRect(jugador1.x0, jugador1.y0, jugador1.width, jugador1.height);
+            try
+            {
+                Thread.sleep(50);
+            } catch (InterruptedException ex)
+            {
+                Logger.getLogger(Prueba.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
         }
     }
 }
